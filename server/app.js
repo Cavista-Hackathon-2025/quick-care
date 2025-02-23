c0a0ead972126d4c7ebf3f9f51b03edb47e387a4
Timi16 <timilehinolowu46@gmail.com>
2025-02-23 02:10:12 -0800

    Adding cors

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors
 
 const app = express();
 
 // Middleware
+app.use(cors({ origin: '*' })); // Allow requests from any origin
 app.use(bodyParser.json());
 
 // MongoDB connection
